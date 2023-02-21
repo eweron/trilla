d2712eadaafdbd10f72d8a7723a98d2757f0d7a8
taxbit <taxbit@yandex.ru>
2023-02-21 16:03:01 +0100

    sequelize config fixes

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config({path: '../.env'});
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: "app",
+    password: "secret",
+    database: "app",
+    host: "localhost",
+    dialect: "mysql"
   },
   test: {
     username: "root",
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   }
 }
