8dea6318857a5d1177528c02ad18639f1646deec
taxbit <taxbit@yandex.ru>
2023-02-24 17:15:55 +0100

    userRoles to UserRoles

--- backend/src/models/role.ts
+++ backend/src/models/role.ts
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsToMany(models.User, {
-        through: "userRoles"
+        through: "UserRoles"
       })
     }
   }
